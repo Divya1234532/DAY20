29bce0811806d469105b8882045b8a5f2133bc6c
Divya1234532 <dd1320983@gmail.com>
2026-04-17 10:49:33 +0530

    Implement sample thread class for concurrency example
    Added a sample thread class to demonstrate concurrency.

---
+++ ConcurrencyExample.java
@@ -0,0 +1,21 @@
+class SaampleThread extends Thread{
+    String name;
+    SampleThread(String name)
+    {
+        this.name=name;
+    }
+    public void run()
+    {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(name+":"+i);
+        }
+    }
+}
+public class ConcurrencyExample {
+    public static void main(String[] args) {
+        SampleThread s1=new SampleThread("Task1");
+        SampleThread s2=new SampleThread("Task2");
+
+    }
+}

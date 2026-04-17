5293c68cde6d16853f704d59acfe2ebd58c0a729
Divya1234532 <dd1320983@gmail.com>
2026-04-17 11:01:06 +0530

    Update ConcurrencyExample.java

--- ConcurrencyExample.java
+++ ConcurrencyExample.java
@@ -1,21 +1,23 @@
-class SaampleThread extends Thread{
+class Sample extends Thread {
     String name;
-    SampleThread(String name)
-    {
-        this.name=name;
+
+    Sample(String name) {
+        this.name = name;
     }
-    public void run()
-    {
-        for(int i=0;i<=5;i++)
-        {
-            System.out.println(name+":"+i);
+
+    public void run() {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(name + ":" + i);
         }
     }
 }
+
 public class ConcurrencyExample {
     public static void main(String[] args) {
-        SampleThread s1=new SampleThread("Task1");
-        SampleThread s2=new SampleThread("Task2");
+        Sample s1 = new Sample("Task1");
+        Sample s2 = new Sample("Task2");
 
+        s1.start();
+        s2.start();
     }
 }
